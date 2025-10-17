1af2ef84c4f3ea714571faa5ff7d8cceda673b9a
Kati957 <shin.map957@gmail.com>
2025-10-17 15:40:03 +0700

    TestAuth

---
+++ PTJ_API/Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PTJ_API.Controllers
+{
+    public class Auth : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PTJ_Models/DTO/Auth/TokenResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTJ_Models.DTO.Auth
+{
+    internal class TokenResponseDto
+    {
+    }
+}
