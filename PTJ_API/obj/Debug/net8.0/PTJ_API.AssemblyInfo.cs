13aca563ef767038480b319eec0df02c8cd124e7
khaioday <minhkhai102003@gmail.com>
2025-10-20 17:13:29 +0700

    pull

--- PTJ_API/obj/Debug/net8.0/PTJ_API.AssemblyInfo.cs
+++ PTJ_API/obj/Debug/net8.0/PTJ_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTJ_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2745c918dc59d2d3d470f1faf06705e0f2048595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d119819f00ca350149e03ab49b24e0f6369fc7df")]
 [assembly: System.Reflection.AssemblyProductAttribute("PTJ_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PTJ_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
