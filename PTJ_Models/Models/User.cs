d186f63097d74df2d6c03e28c299356b40e544dc
Kati957 <shin.map957@gmail.com>
2025-10-27 22:53:20 +0700

    UpdateModel2

--- PTJ_Models/Models/User.cs
+++ PTJ_Models/Models/User.cs
@@ -13,7 +13,7 @@ public partial class User
 
     public string Email { get; set; } = null!;
 
-    public string? PhoneNumber { get; set; }
+    public int? PhoneNumber { get; set; }
 
     public string? Address { get; set; }
 
