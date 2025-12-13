b60604eb7d30d429e26b7ea5972ddd391d4b6f54
hoang-he176577 <hoangkudokai@gmail.com>
2025-12-13 13:25:57 +0700

    update(cv)

--- PTJ_Models/DTO/CvDTO/JobSeekerCvCreateDto.cs
+++ PTJ_Models/DTO/CvDTO/JobSeekerCvCreateDto.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace PTJ_Models.DTO.CvDTO
+public class JobSeekerCvCreateDto
 {
-    public class JobSeekerCvCreateDto
-    {
-        [Required(ErrorMessage = "Please enter a Title")]
-        public string CvTitle { get; set; } = null!;
+    [Required(ErrorMessage = "Vui lòng nhập tiêu đề CV")]
+    public string CvTitle { get; set; } = null!;
 
-        [MaxLength(1000, ErrorMessage = "Skill summary too long")]
-        public string? SkillSummary { get; set; }
+    [MaxLength(1000, ErrorMessage = "Tóm tắt kỹ năng quá dài (tối đa 1000 ký tự)")]
+    public string? SkillSummary { get; set; }
 
-        public string? Skills { get; set; }
+    public string? Skills { get; set; }
 
-        public string? PreferredJobType { get; set; }
+    public string? PreferredJobType { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Please select province/city")]
-        public int ProvinceId { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn Tỉnh/Thành phố")]
+    public int ProvinceId { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Please select district")]
-        public int DistrictId { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn Quận/Huyện")]
+    public int DistrictId { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Please select ward/commune")]
-        public int WardId { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn Phường/Xã")]
+    public int WardId { get; set; }
 
-        [Required(ErrorMessage = "Please enter PhoneNumber")]
-        [RegularExpression(@"^[0-9]{9,11}$", ErrorMessage = "Invalid PhoneNumber")]
-        public string? ContactPhone { get; set; }
-    }
+    [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+    [RegularExpression(@"^[0-9]{9,11}$", ErrorMessage = "Số điện thoại không hợp lệ")]
+    public string? ContactPhone { get; set; }
 }
